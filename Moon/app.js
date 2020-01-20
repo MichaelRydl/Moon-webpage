16e00f6f1e20ef629c670c57fec577c352cd451a
Michael Rýdl <29844074+MichaelRydl@users.noreply.github.com>
2020-01-20 15:27:51 +0100

    Add files via upload

--- Moon/app.js
+++ Moon/app.js
@@ -1,52 +1,52 @@
 let sizeOfTheMenu = window.innerWidth;
 
-if (sizeOfTheMenu >= 961) {
-  function openNav() {
-    document.getElementById("sideNav").style.width = "30vw";
+if ( sizeOfTheMenu >= 961 ) {
+  function openNav () {
+    document.getElementById( "sideNav" ).style.width = "30vw";
   }
-  function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
+  function closeNav () {
+    document.getElementById( "sideNav" ).style.width = "0";
   }
 } else {
-  function openNav() {
-    document.getElementById("sideNav").style.width = "100%";
-}
-  function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
+  function openNav () {
+    document.getElementById( "sideNav" ).style.width = "100%";
+  }
+  function closeNav () {
+    document.getElementById( "sideNav" ).style.width = "0";
   }
 }
 
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
+$( document ).ready( function () {
+  $( "a" ).on( 'click', function ( event ) {
+    if ( this.hash !== "" ) {
+      event.preventDefault();
+      var hash = this.hash;
 
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 900, function(){
-             window.location.hash = hash;
-        });
-      }
-    });
-});
+      $( 'html, body' ).animate( {
+        scrollTop: $( hash ).offset().top
+      }, 900, function () {
+        window.location.hash = hash;
+      } );
+    }
+  } );
+} );
 
-function initMap() {
-  var uluru = {lat: 50.075538, lng: 14.4378};
-  var map = new google.maps.Map(document.getElementById('map'), {
+function initMap () {
+  var uluru = { lat: 50.075538, lng: 14.4378 };
+  var map = new google.maps.Map( document.getElementById( 'map' ), {
     zoom: 10,
     center: uluru
-  });
-  var marker = new google.maps.Marker({
+  } );
+  var marker = new google.maps.Marker( {
     position: uluru,
     map: map
-  });
+  } );
 }
 
 /*
     $(".europa-moon").click(function() {
       $(this).css("z-index", "100");
-      $(this).css("transform", "scale(1.3)"); 
+      $(this).css("transform", "scale(1.3)");
     });
 
     $(".europa-moon").mouseout(function () {
@@ -60,7 +60,7 @@ function initMap() {
       $(this).animate({right: 420}, 'slow');
       $(this).css("transform", "scale(1.3)");
     });
-    
+
     $(".rhea-moon").dblclick(function () {
       $(this).css("z-index", "0");
       $(this).css("position", "none");
@@ -70,9 +70,9 @@ function initMap() {
 
     $(".ganymede-moon").click(function() {
       $(this).css("z-index", "100");
-      $(this).css("transform", "scale(1.3)"); 
+      $(this).css("transform", "scale(1.3)");
     });
-    
+
     $(".ganymede-moon").mouseout(function () {
       $(this).css("z-index", "0");
       $(this).css("transform", "scale(1)");
